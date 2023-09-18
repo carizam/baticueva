01512a92b6acaa6b0b256ca03264e20c6bb67514
Carlos Ariza <ciarizam@hotmail.com>
2023-09-18 12:17:40 -0500

    Bug del carrito de compras arreglado

--- script.js
+++ script.js
@@ -1,39 +1,18 @@
-window.onload = function () {
-  const carritoGuardado = localStorage.getItem("carrito");
-  if (carritoGuardado) {
-    carrito = JSON.parse(carritoGuardado);
-    actualizarCarrito();
-    actualizarBurbujaCarrito();
-  }
-};
-
-let carrito = []; // Array para almacenar elementos del carrito
-
-// Comprobar si hay algo en localStorage al cargar la página
-if (localStorage.getItem("carrito")) {
-  carrito = JSON.parse(localStorage.getItem("carrito"));
-  actualizarBurbujaCarrito(); // Para actualizar la burbuja al cargar la página con el número de ítems en el carrito
-}
+const carrito = JSON.parse(localStorage.getItem("carrito")) || []; // Iniciar carrito desde localStorage o usar un array vacío
 
 // Función para agregar un elemento al carrito
 function agregarAlCarrito(nombre, precio) {
-  const comicExistente = carrito.find((item) => item.nombre === nombre); // Buscar si el cómic ya está en el carrito
+  const comicExistente = carrito.find((item) => item.nombre === nombre);
 
-  // Si el cómic ya existe en el carrito, aumenta su cantidad
   if (comicExistente) {
     comicExistente.cantidad++;
   } else {
-    // Si no existe, agrega un nuevo objeto al carrito con ese cómic
     carrito.push({ nombre, precio, cantidad: 1 });
   }
 
-  localStorage.setItem("carrito", JSON.stringify(carrito));
-
-  // Mostrar el Toast
   mostrarToast();
-
-  // Actualizar la burbuja del carrito con la cantidad total de ítems
   actualizarBurbujaCarrito();
+  guardarCarritoEnLocalStorage();
 }
 
 // Función para mostrar el Toast de "Producto añadido"
@@ -47,20 +26,18 @@ function mostrarToast() {
 
 // Función para actualizar la burbuja del carrito en la barra de navegación
 function actualizarBurbujaCarrito() {
-  const burbujaCarrito = document.getElementById("burbuja-carrito"); // Obtener el elemento HTML de la burbuja
+  const burbujaCarrito = document.getElementById("burbuja-carrito");
   const totalCantidad = carrito.reduce(
     (total, item) => total + item.cantidad,
     0
-  ); // Calcular la cantidad total de ítems
-  burbujaCarrito.textContent = totalCantidad > 0 ? totalCantidad : ""; // Si hay ítems, muestra la cantidad, si no, deja vacío
+  );
+  burbujaCarrito.textContent = totalCantidad > 0 ? totalCantidad : "";
 }
 
 function actualizarCarrito() {
-  // 1. Limpiar el contenido actual del carrito
   const carritoModalLista = document.getElementById("carrito-modal");
   carritoModalLista.innerHTML = "";
 
-  // 2. Agregar productos al carrito y calcular el total
   let total = 0;
   carrito.forEach((item) => {
     const li = document.createElement("li");
@@ -77,67 +54,38 @@ function actualizarCarrito() {
     li.appendChild(btnEliminar);
     carritoModalLista.appendChild(li);
 
-    // Sumar al total
     total += item.precio * item.cantidad;
-
-    localStorage.setItem("carrito", JSON.stringify(carrito));
   });
 
-  // 3. Mostrar el total en el carrito
   const totalSpan = document.getElementById("total-modal");
   totalSpan.textContent = total.toFixed(2);
 }
 
 function eliminarDelCarrito(nombre) {
-  // Encuentra el índice del producto en el arreglo carrito basado en su nombre
   const indice = carrito.findIndex((producto) => producto.nombre === nombre);
   if (indice !== -1) {
-    // Si el producto tiene más de una cantidad, reduce esa cantidad
     if (carrito[indice].cantidad > 1) {
       carrito[indice].cantidad--;
     } else {
-      // Si solo hay uno, elimina el producto completamente del carrito
       carrito.splice(indice, 1);
     }
-
-    actualizarBurbujaCarrito();
     actualizarCarrito();
-    mostrarModalCarrito(); // Para refrescar el modal
-    localStorage.setItem("carrito", JSON.stringify(carrito));
+    actualizarBurbujaCarrito();
+    guardarCarritoEnLocalStorage();
   }
 }
 
-// Función para mostrar el modal del carrito al hacer clic en el icono del carrito
+// Función para guardar el carrito en localStorage
+function guardarCarritoEnLocalStorage() {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
 document.getElementById("abrirCarrito").addEventListener("click", function () {
-  const carritoModalLista = document.getElementById("carrito-modal"); // Obtener el elemento HTML donde se listarán los ítems del carrito
-  const totalModalElement = document.getElementById("total-modal"); // Obtener el elemento HTML donde se mostrará el total
-  let totalModal = 0; // Variable para almacenar el total
-
-  // Limpiar la lista modal previa
-  carritoModalLista.innerHTML = "";
-
-  // Recorrer el carrito y agregar elementos a la lista modal
-  carrito.forEach((item) => {
-    const li = document.createElement("li");
-    li.textContent = `${item.cantidad}x ${item.nombre} - $${(
-      item.precio * item.cantidad
-    ).toFixed(2)}`;
-
-    const btnEliminar = document.createElement("button");
-    btnEliminar.textContent = "Eliminar";
-    btnEliminar.onclick = function () {
-      eliminarDelCarrito(item.nombre);
-    };
-
-    li.appendChild(btnEliminar);
-    carritoModalLista.appendChild(li);
-
-    totalModal += item.precio * item.cantidad;
-  });
-
-  // Actualizar el total del modal
-  totalModalElement.textContent = totalModal.toFixed(2);
-
-  // Mostrar el modal usando Bootstrap
+  actualizarCarrito();
   $("#carritoModal").modal("show");
 });
+
+// Al cargar la página, actualizar la burbuja del carrito
+window.onload = function () {
+  actualizarBurbujaCarrito();
+};
